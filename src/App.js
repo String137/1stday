7773c952edd2212b7c493d72e7444c05030ff78c
Hojun <quantum0430@gmail.com>
2021-12-28 05:41:01 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import './App.css';
-
-
+import React from 'react'
 function App() {
+
+  function stringHash(str) {
+    var hash = 0, i, chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+  return hash;
+  }
+  
+  function encode(key) {
+    const str = ">pt_ot]lt";
+    var out = "";
+    for (let i=0; i<str.length; i++){
+      let j = i % key.length;
+      out += String.fromCharCode(str.charCodeAt(i)+key.charCodeAt(j)-50);
+    }
+    return out;
+  }
+
+  function pass(params) {
+    const hashedAnswer = 51665;
+    var curVal = pwInput.current.value;
+    var hashedCurVal = stringHash(curVal)
+    if (hashedAnswer === hashedCurVal){
+      const encoded = encode(curVal)
+      alert('줌 링크가 ' + encoded + '로 바뀌었습니다.');
+    }
+    else{
+      alert("암호가 틀렸습니다.");
+    }
+    return false;
+  }
+  let pwInput = React.createRef();
+
   return (
     <div className="App">
         <div className="top">
@@ -10,11 +46,9 @@ function App() {
         
         <div className="window">
         <form>
-        <input placeholder="암호를 입력하세요." className="password" type="password"/>
-            <button className="button" onclick="
-            alert('test')
-            "> 참가</button>
-            
+        <input ref={pwInput} placeholder="암호를 입력하세요." className="password" type="password" />
+          
+          <button id="button" className="button" onClick={pass}> 참가</button>
         </form>
         
         </div>
